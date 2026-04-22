cc0e809108e5b7c2222162e6571e107d01933c32
OJWatson <o.watson15@imperial.ac.uk>
2026-04-22 10:30:57 +0100

    initial plan

---
+++ analysis/01_download_included_studies.R
@@ -0,0 +1,14 @@
+# 1. Download the screened studies and filter to the included studies
+
+
+# Identify an R package to help download from googledrive
+install.packages("googledrive")
+
+# Download the file
+
+
+# Checks on the file
+
+
+# Filter the file
+saveRDS(df, "analysis/data-derived/included_studies.rds")
---
+++ analysis/plan.md
@@ -0,0 +1,3 @@
+1. Download the screened studies and filter to the included studies
+2. Find dois for the included studies
+3. Use the dois to download pdfs from different databases (?)
